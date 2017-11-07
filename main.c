8b5c38069c492326fccc0480f935e5ba5a891038
Alex Lee <latth5354@gmail.com>
2017-11-07 23:33:28 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int option;
+	
+	printf("\n\t*** Welcome to HK Grand SPACE Movie Ticketing Management System 2017 ***\n");
+	printf("\n\t*** This system is developed by CCIT4020 CLass No.CL-?? Group No.?? ***\n");
+	printf("\n\n--<Basic Functions>--\n");
+	printf("\n 1. Add New Movie Ticketing Record(s):\n");
+	printf("\n 2. Display All Movie Ticketing Records:\n");
+	printf("\n 3. Modify Movie Ticketing Record(s):\n");
+	printf("\n 4. Search Movie Ticketing Record(s):\n");
+	printf("\n 5. Delete Movie Ticketing Record(s):\n");
+	printf("\n What is your option (1-5)? ");
+	scanf("%d", &option);
+}
