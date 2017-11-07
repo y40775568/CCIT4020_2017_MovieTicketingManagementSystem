0cf29849aeac3938be3bfdccda8567e5c7f61bb7
Alex Lee <latth5354@gmail.com>
2017-11-08 00:15:25 +0800

    Main Program
    Updated

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+void menu();
 
 int main()
 {
-	int option;
-	
 	printf("\n\t*** Welcome to HK Grand SPACE Movie Ticketing Management System 2017 ***\n");
 	printf("\n\t*** This system is developed by CCIT4020 CLass No.CL-?? Group No.?? ***\n");
+	menu();
+	
+	return 0;
+}
+
+void menu()
+{
+	char option, phantom;
+	
 	printf("\n\n--<Basic Functions>--\n");
 	printf("\n 1. Add New Movie Ticketing Record(s):\n");
 	printf("\n 2. Display All Movie Ticketing Records:\n");
@@ -13,5 +21,22 @@ int main()
 	printf("\n 4. Search Movie Ticketing Record(s):\n");
 	printf("\n 5. Delete Movie Ticketing Record(s):\n");
 	printf("\n What is your option (1-5)? ");
-	scanf("%d", &option);
+	scanf("%c%c", &option, &phantom);
+	
+	switch(option)
+	{
+		case '1':
+		break;
+		case '2':
+		break;
+		case '3':
+		break;
+		case '4':
+		break;
+		case '5':
+		break;
+		default:
+		printf(" Please select a correct number (1-5)!");
+		menu();
+	}
 }
